06acd1c7ec6c5b9a2450bc15bf0c15624ab52fcd
DESKTOP-PB7NROO\ebyul <ebyul0107@gmail.com>
2022-12-21 11:13:11 +0900

    221221_cartList.html,order.html,orderComplete.html,reser.html,reserComplete.html,CartController,OrderController,ReserController수정_ebko

--- src/main/java/com/ezen/makingbaking/controller/OrderController.java
+++ src/main/java/com/ezen/makingbaking/controller/OrderController.java
@@ -1,8 +1,10 @@
 package com.ezen.makingbaking.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ezen.makingbaking.service.order.OrderService;
 
@@ -11,4 +13,12 @@ import com.ezen.makingbaking.service.order.OrderService;
 public class OrderController {
 	@Autowired
 	private OrderService orderService;
+	
+	@GetMapping("/order")
+	public ModelAndView orderView() {
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName("order/order.html");
+		return mv;
+	}
 }
