f7d65c411387a14110ce4acfe64b4544f6517a8c
seonminiz <sma708@naver.com>
2022-12-27 17:18:14 +0900

    221227_admin&item&ImgFile_asm

--- src/main/java/com/ezen/makingbaking/repository/FileRepository.java
+++ src/main/java/com/ezen/makingbaking/repository/FileRepository.java
@@ -1,10 +1,14 @@
 package com.ezen.makingbaking.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import com.ezen.makingbaking.entity.File;
 import com.ezen.makingbaking.entity.FileId;
+import com.ezen.makingbaking.entity.ImgFile;
 
-public interface FileRepository extends JpaRepository<File, FileId> {
-
+public interface FileRepository extends JpaRepository<ImgFile, FileId> {
+	
+	@Query(value="SELECT IFNULL(MAX(F.FILE_NO), 0) + 1 FROM T_MB_FILE F WHERE F.FILE_NO=:fileNo", nativeQuery=true)
+	int getMaxFileNo(@Param("fileNo") int fileNo);
 }
