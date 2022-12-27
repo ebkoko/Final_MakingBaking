57595459d44f9f19b1c2e3d5e7c686a28954469d
HyeJi-Ahn <kikilove09@naver.com>
2022-12-27 10:02:01 +0900

    221227_boardList paging_hyeji

--- src/main/java/com/ezen/makingbaking/repository/BoardRepository.java
+++ src/main/java/com/ezen/makingbaking/repository/BoardRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ezen.makingbaking.entity.Board;
@@ -11,8 +13,8 @@ import com.ezen.makingbaking.entity.Board;
 @Transactional
 public interface BoardRepository extends JpaRepository<Board, Integer> {
 	// 카테고리 코드로 리스트 검색
+	Page<Board> findByCateCode(int cateCode, Pageable pageable);
+	
 	List<Board> findByCateCode(int cateCode);
-	
-	
 	
 }
--- src/main/java/com/ezen/makingbaking/service/board/BoardService.java
+++ src/main/java/com/ezen/makingbaking/service/board/BoardService.java
@@ -2,11 +2,16 @@ package com.ezen.makingbaking.service.board;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.ezen.makingbaking.entity.Board;
 
 public interface BoardService {
 
 	List<Board> getQnaList(Board board);
+
+	Page<Board> getPageBoardList(Board board, Pageable pageable); //Page<Board> 엔티티로 리턴해줌
 
 
 }
