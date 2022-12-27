47ab01cffefff2f419ceefea4dc705ddb2b4a32a
DESKTOP-PB7NROO\ebyul <ebyul0107@gmail.com>
2022-12-27 17:21:13 +0900

    221227_CartController~~_ebko

--- src/main/java/com/ezen/makingbaking/controller/CartController.java
+++ src/main/java/com/ezen/makingbaking/controller/CartController.java
@@ -1,14 +1,20 @@
 package com.ezen.makingbaking.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ezen.makingbaking.common.CamelHashMap;
 import com.ezen.makingbaking.dto.CartDTO;
+import com.ezen.makingbaking.dto.ResponseDTO;
 import com.ezen.makingbaking.entity.Cart;
 import com.ezen.makingbaking.service.cart.CartService;
 
@@ -21,6 +27,10 @@ public class CartController {
 	@GetMapping("/cartList")
 	public ModelAndView cartListView() {
 		ModelAndView mv = new ModelAndView();
+		
+		List<CamelHashMap> cartList = cartService.getCartList();
+		
+		mv.addObject("getCartList", cartList);
 		
 		mv.setViewName("cart/cartList.html");
 		return mv;
@@ -37,8 +47,27 @@ public class CartController {
 		cartService.insertCart(cart);
 	}
 	
-//	@PostMapping("/deleteCart")
-//	public void deleteCart(@RequestParam("itemNo") int itemNo) {
-//		
-//	}
+	@DeleteMapping("/cart")
+	public ResponseEntity<?> deleteCart(CartDTO cartDTO, Model model) {
+		ResponseDTO<CamelHashMap> response = new ResponseDTO<>();
+		
+		try {
+			Cart cart = Cart.builder()
+							.cartNo(cartDTO.getCartNo())
+							.itemNo(cartDTO.getItemNo())
+							.build();
+			
+			cartService.deleteCart(cart);
+			
+			List<CamelHashMap> cartList = cartService.getCartList();
+			response.setItems(cartList);
+			
+			model.addAttribute("getCartList", cartList);
+			
+			return ResponseEntity.ok().body(response);
+		} catch(Exception e) {
+			response.setErrorMessage(e.getMessage());
+			return ResponseEntity.badRequest().body(response);
+		}
+	}
 }
