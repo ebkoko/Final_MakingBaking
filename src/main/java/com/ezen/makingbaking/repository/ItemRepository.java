b57366a365d9201b1b3f9a161b12c74058710494
seonminiz <sma708@naver.com>
2023-01-04 11:11:04 +0900

    230104_admin/item/header_asm

--- src/main/java/com/ezen/makingbaking/repository/ItemRepository.java
+++ src/main/java/com/ezen/makingbaking/repository/ItemRepository.java
@@ -1,5 +1,7 @@
 package com.ezen.makingbaking.repository;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +12,7 @@ import org.springframework.data.repository.query.Param;
 import com.ezen.makingbaking.common.CamelHashMap;
 import com.ezen.makingbaking.entity.Item;
 
+@Transactional
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 
 	//관리자 상품 검색_선민
@@ -22,12 +25,6 @@ public interface ItemRepository extends JpaRepository<Item, Integer> {
 	@Query(value="SELECT COUNT(*) FROM (SELECT A.*, B.* FROM A.ITEM_NO = B.FILE_NO AND FILE_NO =:fileNo) C", 
 			countQuery="" ,nativeQuery=true)
 	public Page<CamelHashMap> getItemFileList(@Param("fileNo") int fileNo, Pageable pageable);
-	
-	// 관리자 상품update_선민
-	@Modifying
-	@Query(value="UPDATE T_MB_ITEM SET BOARD_CNT = BOARD_CNT + 1 WHERE ITEM_NO = :itemNo", nativeQuery=true)
-	void getUpdateItem(@Param("itemNo") int itemNo);
-	
 	
 	// 장바구니에서 수량 변경할 때 재고보다 많이 변경할 수 없도록 itemNo의 itemStock 조회_은별
 	@Query(value=" SELECT I.ITEM_STOCK FROM T_MB_ITEM I WHERE I.ITEM_NO = :itemNo", nativeQuery = true)
